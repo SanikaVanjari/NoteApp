a8355a8f2cd028d459d107bf011fed58b75658aa
Sanika Vanjari <sanikavanjari94@gmail.com>
2021-08-28 20:57:26 +0530

    Home screen UI updated

--- app/src/main/java/com/practice/noteapp/HomeFragment.kt
+++ app/src/main/java/com/practice/noteapp/HomeFragment.kt
@@ -1,19 +1,20 @@
 package com.practice.noteapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.practice.noteapp.databinding.FragmentFirstBinding
+import com.google.android.material.snackbar.Snackbar
+import com.practice.noteapp.databinding.FragmentHomeBinding
 
 /**
- * A simple [Fragment] subclass as the default destination in the navigation.
+ * Main Screen where all the details would be displayed when filled.
  */
-class FirstFragment : Fragment() {
+class HomeFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +25,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,8 +33,10 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        binding.fab.setOnClickListener { _ ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
 
